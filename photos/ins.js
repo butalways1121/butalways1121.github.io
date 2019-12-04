85e943678f88dabccfc1d7eb59d8b0a0e28566d1
unknown <20180877@jac.net>
2019-12-04 16:00:59 +0800

    Site updated: 2019-12-04 16:00:57

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://gitee.com/butalways1121/photos/raw/master/photos' + data.link[i];
-          var src = 'https://gitee.com/butalways1121/photos/raw/master/min_photos' + data.link[i];
+          var minSrc = 'https://gitee.com/butalways1121/photos/raw/master/photos/' + data.link[i];
+          var src = 'https://gitee.com/butalways1121/photos/raw/master/min_photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
